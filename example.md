0cafb4bc71a882cee67d0240074cb2af552a66fe
James Sartelle <james.sartelle@extra.app>
2023-12-21 21:37:53 -0600

    Initial release

---
+++ example.md
@@ -0,0 +1,32 @@
+# Input
+
+```
+add(add(add(if(equals(outputs('ABC'),null),0,outputs('ABC')),if(equals(outputs('DEF'),null),0,outputs('DEF'))),outputs('GHI')),outputs('JKL'))
+```
+
+# Output
+
+```
+add(
+	add(
+		add(
+			if(
+				equals(
+					outputs('ABC'),
+					null
+				),
+				0,
+				outputs('ABC')
+			),
+			if(
+				equals(
+					outputs('DEF'),
+					null),
+				0,
+				outputs('DEF')
+			)
+		),
+		outputs('GHI')
+	),outputs('JKL')
+)
+```
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,77 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  let input = `add(add(add(if(equals(outputs('ABC'),null),0,outputs('ABC')),if(equals(outputs('DEF'),null),0,outputs('DEF'))),outputs('GHI')),outputs('JKL'))`
+
+  let output: string
+
+  const indentString = '\t'
+
+  function format(input: string): string {
+    let indentLevel = 0
+    const indent = () => indentString.repeat(Math.max(indentLevel, 0))
+    return input.replaceAll(/[\(\)],?|,/g, (match) => {
+      if (match.startsWith('(')) {
+        indentLevel++
+        return match + '\n' + indent()
+      } else if (match.startsWith(')')) {
+        indentLevel--
+        return '\n' + indent() + match + '\n' + indent()
+      } else if (match === ',') {
+        return match + '\n' + indent()
+      } else {
+        return match
+      }
+    })
+  }
+  $: output = format(input)
+
+  let copied = false
+  async function copyOutput() {
+    await navigator.clipboard.writeText(output)
+    copied = true
+    setTimeout(() => (copied = false), 3000)
+  }
+</script>
+
+<main>
+  <h1>Bracket Balancer</h1>
+
+  <label class="input">
+    <span>Input</span>
+    <textarea bind:value={input}></textarea>
+  </label>
+
+  <label class="output">
+    <span>Output</span>
+    <div>
+      <button on:click={copyOutput}>{copied ? '✓' : 'Copy'}</button>
+      <textarea readonly bind:value={output}></textarea>
+    </div>
+  </label>
+</main>
+
+<style>
+  textarea {
+    height: 200px;
+    font-family: monospace;
+    white-space: pre-wrap;
+    resize: vertical;
+  }
+
+  .output {
+    & div {
+      position: relative;
+    }
+
+    & button {
+      display: inline;
+      width: auto;
+      position: absolute;
+      top: var(--form-element-spacing-vertical);
+      right: var(--form-element-spacing-horizontal);
+    }
+
+    & textarea {
+      height: 500px;
+    }
+  }
+</style>
